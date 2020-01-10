24767a5d54ea0bc692609bd339477cef06cb4276
Philippe Thierry <phil@reseau-libre.net>
2020-01-10 08:14:59 +0100

    [enhancement] add support to set registers values and fields

--- svd-dump.py
+++ svd-dump.py
@@ -56,6 +56,7 @@ To turn on colorizing non-zero values, set colorize = True
 (doesn't work in TUI mode)
 """
 
+import sys
 import gdb
 import struct
 from cmsis_svd.parser import SVDParser
@@ -104,6 +105,7 @@ class SVDSelector(gdb.Command):
                 try:
                     parser = SVDParser.for_packaged_svd(vendor_name, filename)
                     _svd_printer.set_device(parser.get_device())
+                    _svd_setter.set_device(parser.get_device())
                 except IOError:
                     raise gdb.GdbError("Failed to load SVD file")
                 else:
@@ -127,6 +129,7 @@ class SVDLoader(gdb.Command):
         try:
             parser = SVDParser.for_xml_file(path)
             _svd_printer.set_device(parser.get_device())
+            _svd_setter.set_device(parser.get_device())
         except IOError:
             raise gdb.GdbError("Failed to load SVD file")
         else:
@@ -145,7 +148,7 @@ class SVDPrinter(gdb.Command):
     def set_device(self, device):
         self.device = device
         self.peripherals = dict((peripheral.name,peripheral) for peripheral in self.device.peripherals)
-        
+
     def complete(self, text, word):
         if not self.device:
             return gdb.COMPLETE_NONE
@@ -183,7 +186,7 @@ class SVDPrinter(gdb.Command):
             return register_names
         return gdb.COMPLETE_NONE
 
-    def dump_register(self, peripheral, register, name_width=0, options=""):
+    def dump_register(self, peripheral, register, name_width=0, options="", asked_field=""):
         if not name_width:
             name_width = len(register.name)
         val = struct.unpack("<L", gdb.inferiors()[0].read_memory(peripheral.base_address + register.address_offset, 4))[0];
@@ -207,6 +210,7 @@ class SVDPrinter(gdb.Command):
         else:
             field_fmt = "{name}={value:d}"
             active_field_fmt = "\033[32m{name}={value:d}\033[0m"
+        found_field = False
         for field in register.fields:
             fieldval = (val >> field.bit_offset) & ((1 << field.bit_width) - 1)
             hex_width = (field.bit_width + 3) // 4
@@ -214,12 +218,16 @@ class SVDPrinter(gdb.Command):
                 fmt = active_field_fmt
             else:
                 fmt = field_fmt
-            
-            print(fmt.format(name=field.name,
+            if asked_field == "" or field.name == asked_field:
+                found_field = True
+                print(fmt.format(name=field.name,
                              value=fieldval,
                              bit_width=field.bit_width,
                              hex_width=hex_width)),
         print
+        if found_field == False:
+            raise gdb.GdbError("Invalid field name "+asked_field)
+
     def invoke (self, arg, from_tty):
         try:
             if not self.device:
@@ -242,7 +250,7 @@ class SVDPrinter(gdb.Command):
                 if len(args) == 1:
                     print("%s @ 0x%08x" % (peripheral.name, peripheral.base_address))
                     if peripheral.registers:
-                        width = max(len(reg.name) for reg in peripheral.registers) 
+                        width = max(len(reg.name) for reg in peripheral.registers)
                         for register in peripheral.registers:
                             self.dump_register(peripheral, register, width, options)
                 elif len(args) == 2:
@@ -252,11 +260,181 @@ class SVDPrinter(gdb.Command):
                             break
                     else:
                         raise gdb.GdbError("Invalid register name")
+                elif len(args) == 3:
+                    for register in peripheral.registers:
+                        if register.name == args[1]:
+                            self.dump_register(peripheral, register, 0, options, asked_field=args[2])
+                            break
+                    else:
+                        raise gdb.GdbError("Invalid register name")
             else:
                 raise gdb.GdbError("Usage: svd_show[/[x|b]fi] peripheral-name [register-name]")
         except KeyboardInterrupt:
             pass
 
+##### Adding a new command to set registers
+class SVDSet(gdb.Command):
+    colorize = False
+    def __init__ (self, device=None):
+        super (SVDSet, self).__init__ ("svd_set", gdb.COMMAND_USER)
+        self.device = device
+        if device:
+            self.peripherals = dict((peripheral.name,peripheral) for peripheral in self.device.peripherals)
+        else:
+            self.periphrals = {}
+
+    def set_device(self, device):
+        self.device = device
+        self.peripherals = dict((peripheral.name,peripheral) for peripheral in self.device.peripherals)
+
+    def complete(self, text, word):
+        if not self.device:
+            return gdb.COMPLETE_NONE
+        args = gdb.string_to_argv(text)
+        # Skip over the /x in "svd_set/x"
+        if text.startswith("/"):
+            options = args[0][1:]
+            args = args[1:]
+            if text.startswith("/"+options):
+                text = text[1+len(options):]
+            if word.startswith(options):
+                word = ""
+
+        num_args = len(args)
+        if text.endswith(" "):
+            num_args += 1
+        if not text:
+            num_args = 1
+
+        if num_args == 1:
+            peripheral_names = [peripheral.name.upper() for peripheral in self.device.peripherals]
+            if word:
+                prefix = word.upper()
+                return [name for name in peripheral_names if name.startswith(prefix)]
+            else:
+                return peripheral_names
+        elif num_args == 2 and args[0].upper() in self.peripherals:
+            periph_name = args[0].upper()
+            periph = self.peripherals[periph_name]
+            register_names = [register.name for register in periph.registers]
+            if word:
+                prefix = word.upper()
+                return [name for name in register_names if name.upper().startswith(prefix)]
+            return register_names
+        return gdb.COMPLETE_NONE
+
+
+    def set_register(self, peripheral, register, selected_field, newvalue, name_width=0, options=""):
+        if not name_width:
+            name_width = len(register.name)
+        val = struct.unpack("<L", gdb.inferiors()[0].read_memory(peripheral.base_address + register.address_offset, 4))[0]
+        for field in register.fields:
+            fieldval = (val >> field.bit_offset) & ((1 << field.bit_width) - 1)
+            hex_width = (field.bit_width + 3) // 4
+            # does the current field match the field to set ?
+            if field.name == selected_field:
+                # create the field mask
+                msk = (0x1 << (field.bit_width)) - 1
+                msk = msk << field.bit_offset
+                # zeroify the field
+                tmp = val & ~msk
+                # add the new value
+                val = (tmp | (int(newvalue, 0) << field.bit_offset))
+        # now pack the struct to set the register with the new field value
+        if sys.version_info[0] < 3:
+            # python 2 gdb API
+           gdb.inferiors()[0].write_memory(peripheral.base_address + register.address_offset, str(struct.pack('<I', val)))
+        else:
+            # python 3 gdb API
+           gdb.inferiors()[0].write_memory(peripheral.base_address + register.address_offset, bytearray(struct.pack('<I', val)))
+    def invoke (self, arg, from_tty):
+        try:
+            if not self.device:
+                raise gdb.GdbError("Use svd_load to load an SVD file first")
+                return
+
+            args = gdb.string_to_argv(arg)
+
+            # Extract formatting options
+            options = ""
+            if args and args[0].startswith("/"):
+                options = args[0]
+                args = args[1:]
+
+            if len(args) == 4:
+                if args[0] not in self.peripherals:
+                    raise gdb.GdbError("Invalid peripheral name")
+                    return
+                peripheral = self.peripherals[args[0]]
+                for register in peripheral.registers:
+                    if register.name == args[1]:
+                        selected_register = register
+                        break
+                else:
+                    raise gdb.GdbError("Invalid register name")
+                for field in selected_register.fields:
+                    if field.name == args[2]:
+                        selected_field = args[2]
+                        break
+                else:
+                    raise gdb.GdbError("Invalid field name")
+                # everything is okay, let's set register field
+                self.set_register(peripheral, selected_register, selected_field, args[3], options)
+            else:
+                raise gdb.GdbError("Usage: svd_set peripheral-name register-name fieldname value")
+        except KeyboardInterrupt:
+            pass
+
+
+
+# Handle Python 2/3 issues
+def is_python_2():
+    if sys.version_info[0] < 3:
+        return True
+    else:
+        return False
+
+# Helper to ask the user for something
+def get_user_input(prompt):
+    # Handle the Python 2/3 issue
+    if is_python_2() == False:
+        return input(prompt)
+    else:
+        return raw_input(prompt)
+
+def ask_user_confirmation(asking):
+    asked = ""
+    prompt = "You asked to "+asking+". Enter y to confirm, n to cancel [y/n]. "
+    while asked != "y" and asked != "n":
+        print(prompt)
+        asked = get_user_input(prompt)
+    return asked
+
+##### Adding a new command to check user confirmation
+class SVDConfirm(gdb.Command):
+    colorize = False
+    def __init__ (self):
+        super (SVDConfirm, self).__init__ ("svd_confirm", gdb.COMMAND_USER)
+
+    def invoke (self, arg, from_tty):
+        try:
+            args = gdb.string_to_argv(arg)
+            answer = ask_user_confirmation(args[0])
+            if answer == "y":
+               print("User confirmed action ...")
+            elif answer == "n":
+               raise gdb.GdbError("User declined action ... Aborting")
+            else:
+                return
+        except KeyboardInterrupt:
+            pass
+
+
+
+
+
 SVDSelector()
 SVDLoader()
 _svd_printer = SVDPrinter()
+_svd_setter = SVDSet()
+_svd_confirm = SVDConfirm()
